fa66b56fa9c71af3b4c1f98a4c1eaa786bb39a0e
Brian <briantkho@gmail.com>
2022-05-29 02:08:56 -0700

    completed front end of site

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import { useFonts } from "expo-font";
@@ -5,19 +6,23 @@ import { useFonts } from "expo-font";
 import Home from "./screens/Home";
 import Details from "./screens/Details";
 
-const Stack = createStackNavigator();
-
 const theme = {
   ...DefaultTheme,
-  colors: { ...DefaultTheme.colors, background: "transparent" },
+  colors: {
+    ...DefaultTheme.colors,
+    background: "transparent",
+  },
 };
+
+const Stack = createStackNavigator();
+
 const App = () => {
   const [loaded] = useFonts({
     InterBold: require("./assets/fonts/Inter-Bold.ttf"),
-    InterLight: require("./assets/fonts/Inter-Light.ttf"),
+    InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
     InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
     InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
-    InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
+    InterLight: require("./assets/fonts/Inter-Light.ttf"),
   });
 
   if (!loaded) return null;
